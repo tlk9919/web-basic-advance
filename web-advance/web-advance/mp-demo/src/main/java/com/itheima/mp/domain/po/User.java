a5321bd845ab689c02fcf0a4951841e9814e44db
tlk9919 <tlk_sure@126.com>
2025-03-19 22:40:15 +0800

    微服务基本-mp-枚举处理器

--- web-advance/web-advance/mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
+++ web-advance/web-advance/mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
@@ -3,6 +3,7 @@ package com.itheima.mp.domain.po;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.itheima.mp.enums.UserStatus;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -40,7 +41,7 @@ public class User {
     /**
      * 使用状态（1正常 2冻结）
      */
-    private Integer status;
+    private UserStatus status;
 
     /**
      * 账户余额
---
+++ web-advance/web-advance/mp-demo/src/main/java/com/itheima/mp/enums/UserStatus.java
@@ -0,0 +1,19 @@
+package com.itheima.mp.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.Getter;
+
+@Getter
+public enum UserStatus {
+    Normal(1, "正常"),
+    FROZEN(2, "冻结");
+
+    @EnumValue
+    private final int value;
+    private final String desc;
+
+    UserStatus(int value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+}
